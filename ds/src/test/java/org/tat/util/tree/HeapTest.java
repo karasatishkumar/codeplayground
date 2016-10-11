fa478580bce44c3bdc5b9faaab7a2cb52437cb04
Kara Satish Kumar <karasatishkumar@gmail.com>
2016-10-11 23:37:06 +0530

    Heap insert test cases

--- ds/src/test/java/org/tat/util/tree/HeapTest.java
+++ ds/src/test/java/org/tat/util/tree/HeapTest.java
@@ -14,7 +14,6 @@ public class HeapTest {
 
 	@Before
 	public void setUp() throws Exception {
-		heap = new Heap<>(Type.MIN);
 	}
 
 	@After
@@ -22,7 +21,8 @@ public class HeapTest {
 	}
 
 	@Test(expected = RuntimeException.class)
-	public void testInsertExp() {
+	public void testInsertExpMin() {
+		heap = new Heap<>(Type.MIN);
 		heap.insert(1);
 		heap.insert(2);
 		heap.insert(3);
@@ -39,7 +39,8 @@ public class HeapTest {
 	}
 	
 	@Test
-	public void testInsert1() {
+	public void testInsert1Min() {
+		heap = new Heap<>(Type.MIN);
 		heap.insert(1);
 		heap.insert(2);
 		heap.insert(3);
@@ -54,7 +55,8 @@ public class HeapTest {
 	}
 	
 	@Test
-	public void testInsert2() {
+	public void testInsert2Min() {
+		heap = new Heap<>(Type.MIN);
 		heap.insert(2);
 		heap.insert(4);
 		heap.insert(3);
@@ -66,6 +68,56 @@ public class HeapTest {
 		heap.insert(9);
 		heap.insert(7);
 		Assert.assertEquals(heap.getStore(), new Object[]{1, 2, 3, 4, 6, 8, 5, 10, 9, 7});
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testInsertExpMax() {
+		heap = new Heap<>(Type.MAX);
+		heap.insert(1);
+		heap.insert(2);
+		heap.insert(3);
+		heap.insert(4);
+		heap.insert(5);
+		heap.insert(6);
+		heap.insert(7);
+		heap.insert(8);
+		heap.insert(9);
+		heap.insert(10);
+		heap.insert(11);
+		heap.insert(12);
+		System.out.println(heap);
+	}
+	
+	@Test
+	public void testInsert1Max() {
+		heap = new Heap<>(Type.MAX);
+		heap.insert(1);
+		heap.insert(2);
+		heap.insert(3);
+		heap.insert(4);
+		heap.insert(5);
+		heap.insert(6);
+		heap.insert(7);
+		heap.insert(8);
+		heap.insert(9);
+		heap.insert(10);
+		Assert.assertEquals(heap.getStore(), new Object[]{10, 9, 6, 7, 8, 2, 5, 1, 4, 3});
+	}
+	
+	@Test
+	public void testInsert2Max() {
+		heap = new Heap<>(Type.MAX);
+		heap.insert(2);
+		heap.insert(4);
+		heap.insert(3);
+		heap.insert(10);
+		heap.insert(6);
+		heap.insert(8);
+		heap.insert(5);
+		heap.insert(1);
+		heap.insert(9);
+		heap.insert(7);
+		Assert.assertEquals(heap.getStore(), new Object[]{10, 9, 8, 6, 7, 3, 5, 1, 2, 4});
 	}
 
 	//@Test
