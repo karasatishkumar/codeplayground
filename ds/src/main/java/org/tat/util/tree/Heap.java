b831b2a24ae8cc8e4a6791a1c5c960f455858bc9
Kara Satish Kumar <karasatishkumar@gmail.com>
2016-10-11 23:31:24 +0530

    insert in to heap.. Array implementation

--- ds/src/main/java/org/tat/util/tree/Heap.java
+++ ds/src/main/java/org/tat/util/tree/Heap.java
@@ -1,38 +1,85 @@
 package org.tat.util.tree;
 
-public class Heap <T extends Object & Comparable>{
-	private Node<T> root;
+import java.util.Arrays;
+
+public class Heap<T extends Object & Comparable> {
+	private Object[] store;
 	private Type type;
-	
+	private int index;
+	private int size;
+
 	public Heap(Type type) {
-		root = null;
+		this(type, 10);
+	}
+
+	public Heap(Type type, int size) {
+		this.store = new Object[size];
+		this.index = -1;
 		this.type = type;
+		this.size = size;
 	}
-	
-	public void insert(T key){
-		Node<T> temp = this.insertRec(this.root, key);
-		this.root = temp;
+
+	public void insert(T key) {
+		if (index == size - 1)
+			throw new RuntimeException("Space is full");
+		this.store[++index] = key;
+		this.heapify(index, Operation.INSERT);
 	}
-	
-	public Node<T> insertRec(Node<T> root, T key){
-		if(root == null){
-			root = new Node<T>(key);
-			return root;
+
+	private void heapify(int i, Operation operation) {
+		if (this.type == Type.MIN) {
+			while (i != 0 && ((T) store[getParent(i)]).compareTo((T) store[i]) > 0) {
+				this.swap(i, getParent(i));
+				i = getParent(i);
+			}
+		} else {
+			while (i != 0 && ((T) store[getParent(i)]).compareTo((T) store[i]) < 0) {
+				this.swap(i, getParent(i));
+				i = getParent(i);
+			}
 		}
-		if(root.getLeft() == null)
-			root.setLeft(insertRec(root.getLeft(), key));
-		else
-			if(root.getRight() == null)
-				root.setRight(insertRec(root.getRight(), key));
-			else
-				root.setLeft(insertRec(root.getLeft(), key));
-		return root;
 	}
-	
-	public void delete(T key){
-		
+
+	private int getParent(int child) {
+		return (child - 1) / 2;
+	}
+
+	private void swap(int i, int j) {
+		T temp = (T) store[i];
+		store[i] = store[j];
+		store[j] = temp;
+	}
+
+	public void delete(T key) {
+
+	}
+
+	public Object[] getStore() {
+		return store;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	@Override
+	public String toString() {
+		return "Heap [store=" + Arrays.toString(store) + " index=" + index;
+	}
+
+	private enum Operation {
+		INSERT, DELETE
 	}
 }
-enum Type{
+
+enum Type {
 	MAX, MIN
 }
--- ds/src/test/java/org/tat/util/tree/HeapTest.java
+++ ds/src/test/java/org/tat/util/tree/HeapTest.java
@@ -3,8 +3,10 @@ package org.tat.util.tree;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 
 public class HeapTest {
 	
@@ -12,15 +14,15 @@ public class HeapTest {
 
 	@Before
 	public void setUp() throws Exception {
-		heap = new Heap<>(Type.MAX);
+		heap = new Heap<>(Type.MIN);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void testInsert() {
+	@Test(expected = RuntimeException.class)
+	public void testInsertExp() {
 		heap.insert(1);
 		heap.insert(2);
 		heap.insert(3);
@@ -33,9 +35,40 @@ public class HeapTest {
 		heap.insert(10);
 		heap.insert(11);
 		heap.insert(12);
+		System.out.println(heap);
+	}
+	
+	@Test
+	public void testInsert1() {
+		heap.insert(1);
+		heap.insert(2);
+		heap.insert(3);
+		heap.insert(4);
+		heap.insert(5);
+		heap.insert(6);
+		heap.insert(7);
+		heap.insert(8);
+		heap.insert(9);
+		heap.insert(10);
+		Assert.assertEquals(heap.getStore(), new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+	}
+	
+	@Test
+	public void testInsert2() {
+		heap.insert(2);
+		heap.insert(4);
+		heap.insert(3);
+		heap.insert(10);
+		heap.insert(6);
+		heap.insert(8);
+		heap.insert(5);
+		heap.insert(1);
+		heap.insert(9);
+		heap.insert(7);
+		Assert.assertEquals(heap.getStore(), new Object[]{1, 2, 3, 4, 6, 8, 5, 10, 9, 7});
 	}
 
-	@Test
+	//@Test
 	public void testDelete() {
 		fail("Not yet implemented");
 	}
