e0f87ba65adb0fc0e823d7bc2639e307bc6be21c
Kara Satish Kumar <karasatishkumar@gmail.com>
2017-01-30 17:50:11 +0530

    string reverse print

--- ds/src/main/java/org/tat/algo/StringReversal.java
+++ ds/src/main/java/org/tat/algo/StringReversal.java
@@ -8,7 +8,17 @@ public class StringReversal {
 		return reverse(input.substring(1))+input.charAt(0);
 	}
 	
+	public static void reversePrint(String input){
+		if(input.length() == 0){
+			return;
+		}
+		reversePrint(input.substring(1));
+		System.out.print(input.charAt(0));
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(reverse("satish"));
+		System.out.println();
+		reversePrint("suchi");
 	}
 }
